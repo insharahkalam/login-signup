0ec1864044bbd0a40f5ccc052dddaab023641f74
INSHARAH <insharahkalam19@gmail.com>
2025-06-28 21:52:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let loginBtn = document.getElementById("loginBtn")
+let form = document.getElementById("form")
+
+
+
+form.addEventListener("submit",function(){
+    alert("You are successfully login..")
+    event.preventDefault()
+     let abc = email.value;
+console.log(abc);
+})
+
+
+
+
+  
